ef565310989460d3329b8ba3a45ae1bc63977d4f
Cody Johnson <codyj@protonmail.com>
2019-08-03 13:36:09 -0400

    Update model

--- client/dm/src/app/models/DungeonMaster.ts
+++ client/dm/src/app/models/DungeonMaster.ts
@@ -1,10 +1,7 @@
-import { Campaign } from './Campaign';
-
 export class DungeonMaster {
   id: number;
   email: string;
   firstName: string;
   lastName: string;
-  currentCampaign: Campaign;
-  campaigns: Campaign[];
+  campaigns: number[];
 }
