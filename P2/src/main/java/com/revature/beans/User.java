d13db1b5d52ebdae568eb42d9a781f75aad62f55
havertok <havertok@gmail.com>
2019-08-03 12:36:14 -0400

    added list of campaign ids to user

--- P2/src/main/java/com/revature/beans/User.java
+++ P2/src/main/java/com/revature/beans/User.java
@@ -1,5 +1,8 @@
 package com.revature.beans;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +15,10 @@ import org.hibernate.annotations.NaturalId;
 
 @Entity
 @Table(name = "USER_TABLE")
-public class User {
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
 	public User() {
 		super();
 	}
@@ -36,6 +42,9 @@ public class User {
 	@NaturalId
 	@Column(name = "USER_EMAIL")
 	private String Email;
+	
+	//List of my column IDs will be passed along with user
+	private List<Integer> campaignIds;
 
 	public int getUserId() {
 		return userId;
@@ -67,6 +76,14 @@ public class User {
 
 	public void setEmail(String email) {
 		Email = email;
+	}
+
+	public List<Integer> getCampaignIds() {
+		return campaignIds;
+	}
+
+	public void setCampaignIds(List<Integer> campaignIds) {
+		this.campaignIds = campaignIds;
 	}
 
 	@Override
