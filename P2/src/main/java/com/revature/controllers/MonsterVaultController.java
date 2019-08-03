12dce36ecb4a18bf791c06301149344e87faf911
LuckyOwlBen <benmalmberg2@gmail.com>
2019-08-03 14:42:25 -0400

    add monsters not working yet

--- P2/src/main/java/com/revature/controllers/MonsterVaultController.java
+++ P2/src/main/java/com/revature/controllers/MonsterVaultController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.revature.beans.MonsterVault;
 import com.revature.services.MonsterVaultService;
@@ -26,13 +28,14 @@ public class MonsterVaultController {
 		return new ResponseEntity<>(mvs.getAllMonsters(),HttpStatus.OK);
 	}
 	
-	@RequestMapping("/addMonster")
+	@RequestMapping(value="/addMonster", method=RequestMethod.POST)
 	public ResponseEntity<String> addMonster(@RequestBody MonsterVault monsterVault){
 		ResponseEntity<String> response = null;
 		try {
 			mvs.addMonster(monsterVault);
 			response = new ResponseEntity<>("Successfully added monster", HttpStatus.OK);
 		}catch(Exception e){
+			e.printStackTrace();
 			response = new ResponseEntity<>("Failed to create monster", HttpStatus.BAD_REQUEST);
 		}
 		return response;
