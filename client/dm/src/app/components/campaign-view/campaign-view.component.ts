42b8f0e74219a0f0ce39a03b20d8657834d118a3
Cody Johnson <codyj@protonmail.com>
2019-08-04 11:40:05 -0400

    Implement entity cards

--- client/dm/src/app/components/campaign-view/campaign-view.component.ts
+++ client/dm/src/app/components/campaign-view/campaign-view.component.ts
@@ -10,6 +10,7 @@ import {Campaign} from '../../models/Campaign';
 })
 export class CampaignViewComponent implements OnInit {
   campaigns: Campaign[] = null;
+  currentCampaign: Campaign = null;
 
   constructor(
     private authService: AuthService,
@@ -17,10 +18,24 @@ export class CampaignViewComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.getCampaigns();
+  }
+
+  getCampaigns() {
     const userId = this.authService.getCurrentDungeonMasterValue().userId;
     this.campaignService.getCampaign(userId).subscribe(campaigns => {
       if (campaigns) {
         this.campaigns = campaigns;
+        this.currentCampaign = this.campaigns[0];
+      }
+    });
+  }
+
+  setCurrentCampaign(id: number) {
+    this.campaigns.forEach(c => {
+      if (c.campaignId === id) {
+        this.currentCampaign = c;
+        return;
       }
     });
   }
