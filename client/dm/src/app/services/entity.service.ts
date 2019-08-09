8b7eeabb6de0408a4c00533726b9f2d50f9a9a73
Cody Johnson <codyj@protonmail.com>
2019-08-09 10:53:39 -0400

    Merge branch 'dev' of https://github.com/1906jun10java/TheDungeonMasters into dev
    # Conflicts:
    Update apiUrls for production

--- client/dm/src/app/services/entity.service.ts
+++ client/dm/src/app/services/entity.service.ts
@@ -4,8 +4,8 @@ import {map} from 'rxjs/operators';
 import {Entity} from '../models/Entity';
 
 // Base URL
-// const apiUrl = 'http://ec2-18-223-30-39.us-east-2.compute.amazonaws.com:8089/P2/activeEntity';
-const apiUrl = 'http://localhost:8080/P2/activeEntity';
+const apiUrl = 'http://ec2-18-223-30-39.us-east-2.compute.amazonaws.com:8089/P2/activeEntity';
+// const apiUrl = 'http://localhost:8080/P2/activeEntity';
 
 // HTTP options
 const httpOptions = {
@@ -23,7 +23,7 @@ export class EntityService {
   ) {}
 
   loadMonsterVault() {
-    const vaultUrl = 'http://localhost:8080/P2/monsterVault/returnAll';
+    const vaultUrl = 'http://ec2-18-223-30-39.us-east-2.compute.amazonaws.com:8089/P2/monsterVault/returnAll';
     return this.http.get<Entity[]>(vaultUrl);
   }
 
--- client/dm/src/app/services/registration.service.ts
+++ client/dm/src/app/services/registration.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 // Base URL
-// const apiUrl = 'http://ec2-18-223-30-39.us-east-2.compute.amazonaws.com:8089/P2/register';
-const url = 'http://localhost:8080/P2/register';
+const apiUrl = 'http://ec2-18-223-30-39.us-east-2.compute.amazonaws.com:8089/P2/register';
+// const apiUrl = 'http://localhost:8080/P2/register';
 
 const httpHeaders = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
@@ -17,9 +17,9 @@ export class RegistrationService {
 
   constructor(private http: HttpClient) { }
 
-  addNewUser(data){
+  addNewUser(data) {
     return this.http.post<string>(
-    url, data, httpHeaders,
-    )
+    apiUrl, data, httpHeaders,
+    );
   }
 }
